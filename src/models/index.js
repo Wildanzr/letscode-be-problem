0eaeb583eaf75f845ae8d97b8372264bdb3227a3
wildanzr <graita.sukma@gmail.com>
2022-12-12 19:55:20 +0700

    feat: remove unused models

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,10 @@
 module.exports = {
-  ...require('./collaboration'),
   ...require('./compete'),
   ...require('./competeProblem'),
-  ...require('./log'),
   ...require('./problem'),
   ...require('./problemSubmission'),
   ...require('./sampleCase'),
   ...require('./submission'),
   ...require('./testCase'),
-  ...require('./user'),
-  ...require('./travelLog')
+  ...require('./user')
 }
